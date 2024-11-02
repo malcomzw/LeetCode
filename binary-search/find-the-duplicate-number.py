8302e8bdd09e1f2850dade3949a1ad35fb70fb14
MALCOM MUDHUNGWAZA <36225044+malcomzw@users.noreply.github.com>
2024-11-02 06:06:32 +0400

    LC-find-the-duplicate-number [Runtime: 31 ms; Memory: 28.4 MB]

--- binary-search/find-the-duplicate-number.py
+++ binary-search/find-the-duplicate-number.py
@@ -1,17 +1,23 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow = fast = nums[0]
+         slow, fast = 0, 0
+         while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+         slow2 = 0
+         while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+        
+       
     
-    while True:
-        slow = nums[slow]       # Move slow by 1 step
-        fast = nums[nums[fast]] # Move fast by 2 steps
-        if slow == fast:        # They meet inside the cycle
-            break
+    
 
-    # Phase 2: Finding the entrance to the cycle (the duplicate)
-    slow = nums[0]  # Reset slow to the start
-    while slow != fast:  # Move both pointers until they meet again
-        slow = nums[slow]
-        fast = nums[fast]
 
-    return slow  # This is the duplicate number
+    
+    
