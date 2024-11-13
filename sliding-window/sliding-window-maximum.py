c09683d9f5b9520d50a69afe016a9c110dd62ef9
MALCOM MUDHUNGWAZA <36225044+malcomzw@users.noreply.github.com>
2024-11-13 13:47:39 +0400

    LC-sliding-window-maximum [Runtime: 145 ms; Memory: 30.4 MB]

--- sliding-window/sliding-window-maximum.py
+++ sliding-window/sliding-window-maximum.py
@@ -1,35 +1,17 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        #edge case , when the list is empty or k = 0
-        if not nums or k == 0:
-            return []
+        res = []
+        q = deque()
 
-        #variables
-        #deque to store the indices 
-        deq = deque()
-        result = []
+        for idx, num in enumerate(nums):
+            while q and q[-1] < num:
+                q.pop()
+            q.append(num)
 
-        #iterate through the array 
-        for i in range(len(nums)):            
-            #if the index at the current element is out of window
-            if deq and deq[0] < i -k +1: #comparing the indices size
-                deq.popleft()   #removing the left most value 
-
-            #remove the elements that are less than the current values
-            while deq and deq[-1] < nums[i]:
-                deq.pop()
-
-            #add the current element to the deque 
-            deq.append(i)
-
-            #append the max value of the window to result 
-            if i > k - 1:
-                result.append(nums[deq[0]])
-        return result
-
-
+            if idx >= k and nums[idx - k] == q[0]:
+                q.popleft()
             
-
+            if idx >= k - 1:
+                res.append(q[0])
         
-
-
+        return res
